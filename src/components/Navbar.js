0db7f18b2f1190c5d7aa0d6510e1fbc74716d784
VisheshRawal <vp2005rawal@gmail.com>
2023-02-23 17:28:54 +0530

    Understand the concept of src folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Hello i am a Navbar</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sunt mollitia, nesciunt corrupti eaque suscipit, recusandae alias nisi, inventore fugiat quidem aliquid aliquam dolore pariatur quaerat assumenda dolor illum dolorum?</div>
   )
 }
